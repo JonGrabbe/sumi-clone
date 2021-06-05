25baf8d24a8a0fb4d475764c585f75b4c57a1134
JonGrabbe <jgrabbe69@gmail.com>
2021-06-04 20:09:04 -0700

    rotated arrow

--- js/main.js
+++ js/main.js
@@ -31,11 +31,16 @@ document.getElementById('menu-button').addEventListener('click', function() {
     }
 })
 
-function updateMaxHeight() {
-    let menuHeight = document.getElementById('header_menu').clientHeight+'px';
-    console.log(menuHeight)
+function updateMaxHeight(height) {
     let menu = document.getElementById('menu-container');
-    menu.style.maxHeight = menuHeight;
+    let menuHeight = menu.style.maxHeight.split('px')[0];
+    menu.style.maxHeight = parseInt(menuHeight)  + parseInt(height) + 'px';
+}
+
+function subtractNewHeight(height) {
+    let menu = document.getElementById('menu-container');
+    let menuHeight = menu.style.maxHeight.split('px')[0];
+    menu.style.maxHeight = parseInt(menuHeight)  - parseInt(height) + 'px';
 }
 
 Array.from(document.getElementsByClassName('header_menu')).forEach(function(item) {
@@ -43,8 +48,21 @@ Array.from(document.getElementsByClassName('header_menu')).forEach(function(item
     Array.from(childrenElments).forEach(function(item) {
         if(item.tagName === 'LI') {           
             item.addEventListener('click', function() {
-                this.classList.toggle('sub-menu-opened');
-                updateMaxHeight()
+                this.classList.toggle('arrow-rotate')
+                let children = this.children;
+                let newHeight = children[1].children[0].clientHeight;
+                // console.log(newHeight)
+                let subMenu = children[1];
+                console.log(subMenu)
+                //set max height on menu-container
+                if(subMenu.style.maxHeight === '0px' || !subMenu.style.maxHeight) {
+                    updateMaxHeight(newHeight)
+                    subMenu.style.maxHeight = newHeight+'px';
+                    } else {
+                    subtractNewHeight(newHeight)
+                    subMenu.style.maxHeight = '0px';
+                }
+                //set max height on sumMenu
             })
         }
     })
