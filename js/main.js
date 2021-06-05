a0f00ee9219b81853c638fcb65b2066475b3df61
JonGrabbe <jgrabbe69@gmail.com>
2021-06-04 19:15:53 -0700

    worked on dropdown menu js file

--- js/main.js
+++ js/main.js
@@ -11,30 +11,41 @@ Array.from(menuButtons).forEach(function(item) {
 
 
 //DROPDOWN MENU
-function openMenu(menu) {
+function openMenu(menu, height) {
+    menu.style.maxHeight = height;    
     setTimeout(function() {
-        menu.style.maxHeight = 'none';
+        // menu.style.maxHeight = 'none';
     }, 300)
+}
+function closeMenu(menu, height) {
+    menu.style.maxHeight = '0px';
 }
 document.getElementById('menu-button').addEventListener('click', function() {
     let menu = document.getElementById('menu-container');
     menu.classList.toggle('open');
     if(menu.classList.contains('open')) {
         let menuHeight = document.getElementById('header_menu').clientHeight+'px';
-        menu.style.maxHeight = menuHeight;
-        // openMenu(menu)
+        //menu.style.maxHeight = menuHeight;
+        openMenu(menu, menuHeight);
     } else {
         menu.style.maxHeight = '0px';
+        // closeMenu(menu, menuHeight);
     }
-    
 })
 
+function updateMaxHeight() {
+    let menuHeight = document.getElementById('header_menu').clientHeight+'px';
+    console.log(menuHeight)
+    let menu = document.getElementById('menu-container');
+    menu.style.maxHeight = menuHeight;
+}
 
 Array.from(document.getElementsByClassName('header_menu')).forEach(function(item) {
     let childrenElments = item.children;
     Array.from(childrenElments).forEach(function(item) {
         if(item.tagName === 'LI') {           
             item.addEventListener('click', function() {
+                updateMaxHeight()
                 this.classList.toggle('sub-menu-opened');
             })
         }
